610d8e7c7c6c72d48ea75f6cd4c3df6906140d57
Benedict <ben.panggabean@gmail.com>
2017-11-27 15:30:01 +0100

    Migrations

--- config/cli-config.php
+++ config/cli-config.php
@@ -6,4 +6,30 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = require __DIR__.'/../src/app.php';
 
-return ConsoleRunner::createHelperSet($app['orm.em']);
+/** @var \Doctrine\ORM\EntityManager $em */
+$em = $app['orm.em'];
+
+$commands[] = new \Doctrine\DBAL\Migrations\Tools\Console\Command\DiffCommand();
+$commands[] = new \Doctrine\DBAL\Migrations\Tools\Console\Command\ExecuteCommand();
+$commands[] = new \Doctrine\DBAL\Migrations\Tools\Console\Command\GenerateCommand();
+$commands[] = new \Doctrine\DBAL\Migrations\Tools\Console\Command\MigrateCommand();
+$commands[] = new \Doctrine\DBAL\Migrations\Tools\Console\Command\StatusCommand();
+$commands[] = new \Doctrine\DBAL\Migrations\Tools\Console\Command\VersionCommand();
+$commands[] = new \Doctrine\DBAL\Migrations\Tools\Console\Command\LatestCommand();
+$commands[] = new \Doctrine\DBAL\Migrations\Tools\Console\Command\UpToDateCommand();
+
+$configuration = new \Doctrine\DBAL\Migrations\Configuration\Configuration($em->getConnection());
+$configuration->setName('SturdyUmbrella Migrations');
+$configuration->setMigrationsTableName('doctrine_migration_versions');
+$configuration->setMigrationsDirectory(__DIR__.'/../src/migrations');
+$configuration->setMigrationsNamespace('SturdyUmbrella\Migrations');
+
+/** @var \Doctrine\DBAL\Migrations\Tools\Console\Command\AbstractCommand $command */
+foreach ($commands as $command) {
+    $command->setMigrationConfiguration($configuration);
+}
+
+$helperSet = ConsoleRunner::createHelperSet($em);
+$helperSet->set(new \Symfony\Component\Console\Helper\QuestionHelper(), 'question');
+
+return $helperSet;
---
+++ src/Migrations/Version20171127142358.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types = 1);
+
+namespace SturdyUmbrella\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171127142358 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('CREATE TABLE User (id INTEGER NOT NULL, name VARCHAR(255) NOT NULL, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE user_user (user_source INTEGER NOT NULL, user_target INTEGER NOT NULL, PRIMARY KEY(user_source, user_target))');
+        $this->addSql('CREATE INDEX IDX_F7129A803AD8644E ON user_user (user_source)');
+        $this->addSql('CREATE INDEX IDX_F7129A80233D34C1 ON user_user (user_target)');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('DROP TABLE User');
+        $this->addSql('DROP TABLE user_user');
+    }
+}
